5252ef1bfb025cb7dafcdd9c9893260c49bddd9a
grantlmiller <gl1miller@yahoo.com>
2022-11-11 11:46:56 -0500

    add scripts

---
+++ RepoToSystem.sh
@@ -0,0 +1,3 @@
+cp ./i3status-rust/config.toml ~/.config/i3status-rust/config.toml
+cp ./i3/config ~/.config/i3/config
+
---
+++ SystemToRepo.sh
@@ -0,0 +1,3 @@
+cp ~/.config/i3status-rust/config.toml ./i3status-rust/config.toml
+cp  ~/.config/i3/config ./i3/config
+
